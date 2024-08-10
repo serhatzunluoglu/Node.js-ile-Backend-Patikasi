a75651e3c54acfae32a3dbc3a9de9f5f1e3b0e13
Serhat İsmail Zunluoğlu <97339604+serhatzunluoglu@users.noreply.github.com>
2024-08-10 15:16:11 +0300

    SQL odev1 uploaded

---
+++ SQL/odev1.sql
@@ -0,0 +1,14 @@
+-- Select title, description FROM film;
+
+-- Select * FROM film
+-- Where length > 60 and length < 75;
+
+-- Select * FROM film
+-- Where rental_rate = 0.99 and (replacement_cost = 12.99 or replacement_cost = 28.99); 
+
+-- Select last_name FROM customer
+-- Where first_name = 'Mary'; 
+
+-- Select * FROM film
+-- Where Not length > 50 and Not (rental_rate = 2.99 or rental_rate= 4.99);
+
