58af5ec203c209704a45e7d2847595c9a993ebde
Serhat İsmail Zunluoğlu <97339604+serhatzunluoglu@users.noreply.github.com>
2024-08-10 15:39:14 +0300

    Add files via upload

---
+++ SQL/odev2.sql
@@ -0,0 +1,8 @@
+-- Select * from film
+-- Where replacement_cost Between 12.99 and 16.99; 
+
+-- Select first_name, last_name from actor
+-- Where first_name In('Penelope', 'Nick', 'Ed');
+
+-- Select * from film
+-- Where rental_rate In(0.99,2.99,4.99) and replacement_cost In(12.99,15.99,28.99);
