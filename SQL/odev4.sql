6018800964d1d1490c99ebee32ee61b5b032b771
Serhat İsmail Zunluoğlu <97339604+serhatzunluoglu@users.noreply.github.com>
2024-08-11 16:27:40 +0300

    Add files via upload

---
+++ SQL/odev4.sql
@@ -0,0 +1,13 @@
+-- Select DISTINCT replacement_cost From film;
+
+-- Select COUNT(DISTINCT replacement_cost) From film;
+
+-- Select Count(*) from film
+-- Where title Like 'T%' and rating = 'G';
+
+-- Select COUNT(*) from country
+-- Where length(country) = 5;
+
+-- Select COUNT(*) from city
+-- Where city Ilike '%r'; 
+
