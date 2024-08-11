f4ca5a55594f72f57ce1ffd4d3001fcb4942b61b
Serhat İsmail Zunluoğlu <97339604+serhatzunluoglu@users.noreply.github.com>
2024-08-11 14:16:01 +0300

    Add files via upload

---
+++ SQL/odev3.sql
@@ -0,0 +1,12 @@
+-- Select country from country
+-- Where country Like 'A%a';
+
+-- Select country from country
+-- Where LENGTH(country) >= 6 and country Like '%n';
+
+-- Select title from film
+-- Where lENGTH(title) >= 4 and title Ilike '%t%';
+
+-- Select * from film
+-- Where length > 90 and title Like 'C%' and rental_rate = 2.99;
+
